d7bbbfdee272006927a0512e40a754264e38bc88
minkika <golubeva.lyubov@gmail.com>
2020-08-19 12:22:39 +0300

    'lesson01'

--- js/main.js
+++ js/main.js
@@ -1,24 +1,164 @@
-const products = [
-  {id: 1, title: 'Notebook', price: 20000},
-  {id: 2, title: 'Mouse', price: 1500},
-  {id: 3, title: 'Keyboard', price: 5000},
-  {id: 4, title: 'Gamepad', price: 4500},
-];
+const products = (() => {
+    const minPrice = 100;
+    const maxPrice = 1000;
+    const names = [
+        "Abby",
+        "Angel",
+        "Annie",
+        "Baby",
+        "Bailey",
+        "Bandit",
+        "Bear",
+        "Bella",
+        "Bob",
+        "Boo",
+        "Boots",
+        "Bubba",
+        "Buddy",
+        "Buster",
+        "Cali",
+        "Callie",
+        "Casper",
+        "Charlie",
+        "Chester",
+        "Chloe",
+        "Cleo",
+        "Coco",
+        "Cookie",
+        "Cuddles",
+        "Daisy",
+        "Dusty",
+        "Felix",
+        "Fluffy",
+        "Garfield",
+        "George",
+        "Ginger",
+        "Gizmo",
+        "Gracie",
+        "Harley",
+        "Jack",
+        "Jasmine",
+        "Jasper",
+        "Kiki",
+        "Kitty",
+        "Leo",
+        "Lilly",
+        "Lily",
+        "Loki",
+        "Lola",
+        "Lucky",
+        "Lucy",
+        "Luna",
+        "Maggie",
+        "Max",
+        "Mia",
+        "Midnight",
+        "Milo",
+        "Mimi",
+        "Miss kitty",
+        "Missy",
+        "Misty",
+        "Mittens",
+        "Molly",
+        "Muffin",
+        "Nala",
+        "Oliver",
+        "Oreo",
+        "Oscar",
+        "Patches",
+        "Peanut",
+        "Pepper",
+        "Precious",
+        "Princess",
+        "Pumpkin",
+        "Rascal",
+        "Rocky",
+        "Sadie",
+        "Salem",
+        "Sam",
+        "Samantha",
+        "Sammy",
+        "Sasha",
+        "Sassy",
+        "Scooter",
+        "Shadow",
+        "Sheba",
+        "Simba",
+        "Simon",
+        "Smokey",
+        "Snickers",
+        "Snowball",
+        "Snuggles",
+        "Socks",
+        "Sophie",
+        "Spooky",
+        "Sugar",
+        "Tiger",
+        "Tigger",
+        "Tinkerbell",
+        "Toby",
+        "Trouble",
+        "Whiskers",
+        "Willow",
+        "Zoe",
+        "Zoey"
+    ];
+    const rndItem = (a) => a[Math.floor(Math.random() * a.length)];
+    const rndFloat = () => (Math.random() * (maxPrice - minPrice) + minPrice).toFixed(2);
+    const rndInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-const renderProduct = (title, price, img = '') => {
-  return `<div class="product-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
-            <button class="by-btn">Добавить в корзину</button>
-          </div>`;
+    const items = [];
+
+    const add = (totalItems) => {
+        while (totalItems--) {
+            items.push({
+                id: totalItems,
+                title: rndItem(names),
+                price: rndFloat(),
+                img: `https://placekitten.com/${rndInt(100, 400)}/${rndInt(200, 600)}`,
+                description: 'Да ведь это же КИСУЛЯ!'
+            });
+        }
+    }
+
+    return {
+        add,
+        getItems: () => items,
+    }
+})();
+
+const renderProduct = ({id = 0, title = '', price = 0, img = '', description = ''}) => {
+    if (!price) {
+        return '';
+    }
+
+    return `
+      <div class="product">
+        <div class="productId">${id}</div>
+        <div class="productAside" style="background-image: url('${img}')"></div>
+        <div class="productDetails">
+            <div class="productData">
+              <div class="productTitle">${title}</div>
+              <div class="productPrice">${price}</div>
+            </div>
+            <div class="productControls">
+              <div class="productDescription">${description}</div>
+              <button class="productButton" data-id="${id}">Мне такое надо!</button>
+            </div>
+        </div>
+      </div>
+  `;
 };
 
-const renderProducts = (list) => {
-  const productList = list.map((product) => {
-      return renderProduct(product.title, product.price);
-  });
-  // console.log(productList);
-  document.querySelector('.products').innerHTML = productList;
+const renderProducts = () => {
+    products.add(120);
+    document.querySelector('.products').innerHTML = products.getItems().map(renderProduct).join('');
 }
 
-renderProducts(products);
+renderProducts();
+
+
+document.getElementById('logo')
+    .addEventListener('click', () => {
+    [...document.getElementsByTagName('main')][0].classList.toggle('wide');
+}, false);
