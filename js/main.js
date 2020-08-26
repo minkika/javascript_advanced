982f2a0db5bbf95c82a97afc31fdd8a33a14f0a1
Lyubov Golubeva <golubeva.lyubov@gmail.com>
2020-08-26 23:21:22 +0300

    lesson03

--- js/main.js
+++ js/main.js
@@ -1,99 +1,132 @@
-class ProductItem {
-  constructor(product) {
-    this.title = product.title;
-    this.price = product.price;
-    this.id = product.id;
-    this.img = product.img || 'https://placehold.it/200x150';
-  }
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-  render() {
-    return `
-      <div class="product" data-id="${this.id}">
-        <div class="productDetails" style="background-image: url('${this.img}')">
-              <div class="productTitle">${this.title}</div>
-              <div class="productPrice">${this.price}\u20bd</div>
-              <button class="productButton">В корзину</button>
-        </div>
-        <div class="productId">${this.id}</div>
-      </div>
-    `;
-  }
-}
+// Переделать на Promise (не fetch!!!)
+const getRequest = (url) => new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status !== 200) {
+        reject(xhr.statusText)
+      } else {
+        resolve(JSON.parse(xhr.responseText, null, 2));
+      }
+    }
+  };
+  xhr.send();
+});
 
 class ProductList {
-  #privateProp;
+  #goods;
 
   constructor(container = '.products') {
     this.container = container;
-    this.goods = [];
+    this.#goods = [];
+    this.cart = [];
     this.allProducts = [];
-    this.#privateProp = '123';
+    this.#fetchGoods()
+        .then(data => {
+          this.#goods = [...data];
+          this.#render();
+        });
 
-    this.#fetchProducts();
-    this.render();
-    this.renderTotal();
+    this.addCartItem = this.addCartItem.bind(this);
+    this.removeCartItem = this.removeCartItem.bind(this);
   }
 
-  get prop() {
-    return this.#privateProp;
+  #fetchGoods() {
+    return getRequest(`${API}/catalogData.json`)
+        .then(data => data)
+        .catch(console.log);
   }
 
-  set prop(value) {
-    this.#privateProp = value;
-  }
-
-  #fetchProducts() {
-    this.goods = [
-      {id: 1, title: 'Notebook', price: 20000},
-      {id: 2, title: 'Mouse', price: 1500},
-      {id: 3, title: 'Keyboard', price: 5000, img: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MLA22LL?wid=890&hei=890&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1496944005839' },
-      {id: 4, title: 'Gamepad', price: 4500},
-    ];
-  }
-
-  getTotal() {
-      return this.goods.reduce((a, good) => a + good.price, 0);
-  }
-
-  addProduct(id) {
-      id = parseInt(id, 10);
-
-      this.goods.push(
-          this.goods.find(good => good.id === id)
-      );
-  }
-
-  renderTotal() {
-      document.getElementById('cart-total').innerHTML = `
-        Всего ${this.goods.length} на ${this.getTotal()} \u20bd
-      `;
-  }
-
-  render() {
+  #render() {
     const block = document.querySelector(this.container);
 
-    for (let product of this.goods) {
+    for (let product of this.#goods) {
       const productObject = new ProductItem(product);
 
       this.allProducts.push(productObject);
       block.insertAdjacentHTML('beforeend', productObject.render());
     }
+
+    Array.from(document.querySelectorAll('[data-id]'))
+        .forEach(el => {
+            el.addEventListener('click', (evt) => {
+                const productId = el.dataset.id;
+                if (evt.target.classList.contains('buy-btn')) {
+                    return this.addCartItem(productId);
+                }
+            });
+        });
+  }
+
+  getGoodsPrice() {
+    return this.getCartItems().reduce((sumPrice, { price }) => sumPrice + price, 0);
+  }
+
+  addCartItem(id) {
+      id = parseInt(id, 10);
+      this.cart.push(this.allProducts.find(good => good.id === id));
+      this.renderTotal();
+  }
+
+  findCartItem(id) {
+      if (id) {
+          return this.cart.findIndex(item => item.id === id);
+      }
+      return -1;
+  }
+
+  removeCartItem(id) {
+      id = parseInt(id, 10);
+      const itemPosition = this.findCartItem(id);
+
+      if (itemPosition !== -1) {
+          this.cart.splice(itemPosition, 1);
+          this.renderTotal();
+      }
+  }
+
+  getCartItems() {
+      return this.cart;
+  }
+
+  renderTotal() {
+      document.getElementById('cart-total').innerHTML = `
+        В корзине ${this.getCartItems().length} товаров на ${this.getGoodsPrice()} \u20bd
+      `;
+
+      document.getElementById('cart-items').innerHTML = this.getCartItems()
+          .map((item) => `<li>${item.title} <button data-id="${item.id}" class="remove-btn">x</button></li>`).join('')
+
+        Array.from(document.querySelectorAll('.remove-btn'))
+            .forEach(el => {
+                el.addEventListener('click', () => {
+                    this.removeCartItem(el.dataset.id);
+                })
+            });
+  }
+}
+
+class ProductItem {
+  constructor(product, img='https://placehold.it/200x150') {
+    this.title = product.product_name;
+    this.price = product.price;
+    this.id = product.id_product;
+    this.img = img;
+  }
+
+  render() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+          </div>`;
   }
 }
 
 const list = new ProductList();
-
-Array.from(document.querySelectorAll('.product'))
-    .forEach(el => {
-        el.addEventListener('click', () => {
-            list.addProduct(el.dataset.id);
-            list.renderTotal();
-        });
-    });
-
-
-
-document.getElementById('logo')
-    .addEventListener('click', () => {
-    [...document.getElementsByTagName('main')][0].classList.toggle('wide');
-}, false);
